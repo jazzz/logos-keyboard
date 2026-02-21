36c09b18d39940e9cd6e615ffd902e541c73d9a7
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2026-02-21 08:34:17 -0800

    Package release

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-pkgbuild \
-  --component build/λKeyboard_v$1.bundle \
-  --install-location ~/Library/Keyboard\ Layouts \
-  --identifier io.jazzz.logos-keyboard.keyboardlayouta \
-  --version $1 \
-  λKeyboard_v$1.pkg
-  
